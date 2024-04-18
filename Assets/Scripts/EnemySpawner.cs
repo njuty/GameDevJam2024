29cea605fdeaa2224f5cec6baaf0e46f5a2e67d6
njuty <167004083+njuty@users.noreply.github.com>
2024-04-18 22:33:49 +0200

    feat(spawn): implement spawner (#5)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    //TODO update when wave logic is available
+    [SerializeField]
+    int numberOfEntities = 10;
+
+    [SerializeField]
+    float spawnInterval = 2f;
+
+    [SerializeField]
+    GameObject enemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        StartCoroutine(SpawnEntities());
+    }
+
+    IEnumerator SpawnEntities()
+    {
+        yield return new WaitForSeconds(1f);
+
+        for(int i =0 ; i<numberOfEntities; i++)
+        {
+            Vector2 mainCameraPosition = Camera.main.transform.position;
+
+            float farClipPlane = mainCamera.farClipPlane;
+
+            // Random angle from 0° to 360° (2pi radian = 360°)
+            float randomAngle = Random.Range(0f, Mathf.PI * 2f);
+
+            // farClipPlane with multiplier permit to take into consideration camera settings
+            float x = mainCameraPosition.x + Mathf.Cos(randomAngle) * (farClipPlane * 0.06f);
+            float y = mainCameraPosition.y + Mathf.Sin(randomAngle) * (farClipPlane * 0.06f);
+
+            Vector2 spawnPosition = new Vector2(x, y);
+            Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
