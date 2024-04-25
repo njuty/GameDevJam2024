6579d934413f5d31b788d898562e99556c989f87
njuty <maxime-gourdol@outlook.fr>
2024-04-25 22:05:50 +0200

    feat(potion/player): fix potion size and add death animation on player (wip)

--- Assets/Scripts/RandomPotionSpawner.cs
+++ Assets/Scripts/RandomPotionSpawner.cs
@@ -6,16 +6,12 @@ public class RandomPotionSpawner : MonoBehaviour
     [SerializeField]
     Potion healthPotion;
 
-    [SerializeField]
-    Potion invisibilityPotion;
-
     public float spawnRadius = 30.0f;
     public float minTimeBetweenSpawns = 5.0f;
     public float maxTimeBetweenSpawns = 20.0f;
 
     // Probabilities associated with different potions
-    public float chanceHealthPotion = 0.65f; // 65%
-    public float chanceInvisibilityPotion = 0.35f; // 35%
+    public float chanceHealthPotion = 1f; // 65%
 
     private IEnumerator spawnCoroutine;
 
@@ -46,17 +42,9 @@ public class RandomPotionSpawner : MonoBehaviour
             Vector3 spawnPosition = new Vector3(randomPosition.x, randomPosition.y, 0) + transform.position;
 
             // Determine which potion to spawn
-            float rand = Random.value;
-            Potion potionToSpawn;
-
-            if (rand < chanceHealthPotion)
-            {
-                potionToSpawn = healthPotion;
-            }
-            else
-            {
-                potionToSpawn = invisibilityPotion;
-            }
+            // Uncomment when more than one potion is added
+            //float rand = Random.value;
+            Potion potionToSpawn = healthPotion;
 
             // Spawn the potion at the random position
             if (potionToSpawn != null)
